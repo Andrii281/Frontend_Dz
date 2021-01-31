2fda9a22d317d019831d7a12a3a9c3f59b33105f
_-_-_ <70381657+Andrii281@users.noreply.github.com>
2021-01-31 15:56:18 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import  ShowPost from './components/showPost'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ShowPost/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/showPost/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+
+
+function ShowPost(){
+    return(
+        // <img> src = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg" alt=' '</img>
+
+        <div class = "post">
+            <div class = 'postBody'>
+                <div class = 'photo'>
+                    <img class ="userPhoto" src="https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg" alt=" " />
+                </div>
+                <div class = "block">
+                    <div class = 'info'>
+                        <div class = "name">
+                        Anakin Skywalker
+                        </div>
+                        <div class = "nickname">
+                        @dart_vader
+                        </div>
+                    <div class= "date">
+                    26 февр.
+                    </div>
+                </div>
+            
+                <div class ="content">
+                WTF? Who is Ray? Why she is Skywalker? Luke...?
+                </div>
+            
+            
+            <div class="postPhoto">
+            <img src="https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale" alt=" " />
+            </div>
+            </div>
+            </div>
+        Text Text
+        </div>
+
+    )
+}
+
+
+export default ShowPost;
